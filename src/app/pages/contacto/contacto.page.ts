da8a67933d78fda496dd74669d170bf6f34f59b8
carmelo <carmelo.escribano@gmail.com>
2026-07-01 13:55:33 +0200

    Contacto y Modelo

--- src/app/pages/contacto/contacto.page.ts
+++ src/app/pages/contacto/contacto.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonButton, IonTextarea } from '@ionic/angular/standalone';
+import {Preferences} from '@capacitor/preferences';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonTextarea, IonButton, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ContactoPage implements OnInit {
+
+correo: string="";
+mensaje: string="";
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  async enviar() {
+      await Preferences.set({
+      key: 'ultimoMensaje',
+      value: JSON.stringify({
+      correo:this.correo,
+      mensaje:this.mensaje
+})
+});
+}
 
 }
